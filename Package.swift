8f6b615cc355cca3f2937e26328858636386b8bb
Hugo de Groot <>
2022-05-04 15:33:41 +0200

    no message

--- Package.swift
+++ Package.swift
@@ -41,7 +41,7 @@ let package = Package(
         .target(
             name: "Fastis",
             dependencies: ["JTAppleCalendar", "SnapKit", "PrettyCards"],
-            sources: ["Resources", "Sources", "Derived/Sources"]
+            sources: ["Sources"]
         )
     ],
     swiftLanguageVersions: [.v5]
